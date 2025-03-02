a015ea5d13dc0fe1f10efd0a31b5bb150c90c203
rick <rickbhowmik4.com>
2025-03-02 18:21:04 +0530

    fix: Add Render domain to ALLOWED_HOSTS

--- django_amazon/settings.py
+++ django_amazon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '*@6ae8bwo%r3mhrer#953zp-fa037aygo3*#n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split() or ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['price-jsn3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
