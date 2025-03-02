c6ef388169404bf0a0bb234c001d42bf9a7835c7
rick <rickbhowmik4.com>
2025-03-02 17:56:09 +0530

    Fix Render deployment: Remove apt-get commands, add --prefer-binary flags, and make build script executable

--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies required for pyzmq and other packages
-apt-get update
-apt-get install -y python3-dev build-essential
-
 # Install Python dependencies
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 
 # Run Django migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
